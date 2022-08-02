de710b1e1c810371b1ab3156476c22922234fa71
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-02 16:40:57 +0300

    Task #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryThumbEl = document.querySelector('.gallery');
+
+galleryThumbEl.addEventListener('click', onGalleryItemOpen);
+
+const galleryMarkup = galleryItems.map(galleryElMarkup).join('');
+
+galleryThumbEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function galleryElMarkup({ preview, original, description }) {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+      </a> 
+  </div>`;
+}
+
+function onGalleryItemOpen(e) {
+  e.preventDefault();
+
+  window.addEventListener(
+    'keydown',
+    e => {
+      console.log(e.code);
+      if (e.code === 'Escape') {
+        instance.close();
+      }
+    },
+    { once: true }
+  );
+
+  const instance = basicLightbox.create(`
+<div class="modal">
+    <img
+    class="modal__image"
+    src="${e.target.dataset.source}"
+    />
+</div>
+`);
+
+  instance.show();
+}
