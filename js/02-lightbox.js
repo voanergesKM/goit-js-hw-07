988ef2d8c3c68e09f28ba4e807582b8e7c45a2d2
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-04 15:06:09 +0300

    Task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(getGalleryMarkup).join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+gallery.addEventListener('click', onGalleryItemOpen);
+
+function onGalleryItemOpen(e) {
+  e.preventDefault();
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+function getGalleryMarkup({ preview, original, description }) {
+  return ` 
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `;
+}
